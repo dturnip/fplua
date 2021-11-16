f845540b90eb303f461d9a1923e91b6ef355c9a1
dturnip <alexander.xie007@gmail.com>
2021-11-17 07:43:38 +1100

    docs: add documentation to Iterator class

--- structs/iterator.lua
+++ structs/iterator.lua
@@ -1,3 +1,6 @@
+local error = require("utils.error")
+
+---@class Iterator
 local Iterator = {}
 Iterator.__index = Iterator
 
@@ -29,35 +32,106 @@ local function into_iter(t)
   end
 end
 
+---Instantiates a new Iterator structure from an indexable built in data structure.
+---```lua
+--  local chars_iter = Iterator.from("fplua")
+--  local array_iter = Iterator.from({ 1, 2, 3 })
+--  local map_iter   = Iterator.from({ a = 1, b = 2, c = 3 })
+--```
+---@param t string | table
+---@return Iterator
 function Iterator.from(t)
+  if type(t) ~= "table" and type(t) ~= "string" then
+    error(
+      "Argument `t` passed to Iterator.from(t) must be an Array, Hashmap or String.",
+      2
+    )
+  end
+
   local buf = setmetatable({}, Iterator)
   buf.fn = into_iter(t)
-  buf.t = t
+  buf.obj = t
 
   if type(t) == "table" then
     if #t > 0 then
       -- Array iterator
-      buf.init = 0
+      buf.state = 0
     else
       -- Hashmap iterator
-      buf.init = nil
+      buf.state = nil
     end
   elseif type(t) == "string" then
     -- String iterator
-    buf.init = 0
+    buf.state = 0
   end
 
   return buf
 end
 
+---Pretty prints the Iterator structure to the console.
+---```lua
+--  local xs = Iterator.from({ 1, 2, 3 })
+--  xs:dbg()
+--
+--  -- table: 0x... {
+--  --   [fn] => function: 0x...
+--  --   [state] => 0
+--  --   [obj] => table: 0x... {
+--  --              [1] => 1
+--  --              [2] => 2
+--  --              [3] => 3
+--  --            }
+--  --   }
+--```
 function Iterator:dbg()
   table.display(self)
 end
 
+---Returns an iterator from an Iterator structure which can be used in a generic 'for-in' loop. This method is not suggested to be used directly, but can be if for whatever reason necessary.
+---```lua
+--  local xs = Iterator.from({ 1, 2, 3 })
+--  local sum = 0
+--
+--  for x in xs:iter() do
+--    sum = sum + x
+--  end
+--
+--  print(sum) --> 6
+--```
+---@return fun()
+---@return string | table
+---@return number | nil
 function Iterator:iter()
-  return self.fn, self.t, self.init
+  return self.fn, self.obj, self.state
 end
 
+---Collects the Iterator back into the original data structure. This is done by accessing self.obj on an Iterator structure.
+---```lua
+--  local xs = { 1, 2, 3 }
+--
+--  local new_xs = Iterator.from(xs)
+--		:map(function(x) return x * 2 end)
+--		:map(function(x) return x + 1 end)
+--		:collect() --> { 3, 5, 7 }
+--```
+---@return string | table
+function Iterator:collect()
+  return self.obj
+end
+
+---Applies a function to each yielded element(s) from an Iterator. This method shouldn't be used with intent to map an Iterator, instead it is intended to be used for side effects.
+---```lua
+--  local logins = Iterator.from({ a="a", b="b" })
+--  -- BTW this is just an example! It is an absurd way to store login data!
+--  local file = io.open("logins.txt", "w+")
+--
+--  logins:foreach(function(k, v)
+--		file:write(string.format("username: %s, password: %s\n", k, v))
+--  end)
+--
+--  file:close()
+--```
+---@param fn fun()
 function Iterator:foreach(fn)
   for k, v in self:iter() do
     if v then
@@ -70,9 +144,13 @@ function Iterator:foreach(fn)
   end
 end
 
+---Returns a new Iterator after transforming the values from an Iterator. Since Iterator is a functor, map and alike methods can be chained.
+---@generic K, V
+---@param fn fun(k: K, v: V): any
+---@return Iterator
 function Iterator:map(fn)
-  if type(self.t) ~= "table" then
-    error("[ERROR]: Iterator:map can only be used on table iterators")
+  if type(self.obj) ~= "table" then
+    error("Iterator:map() can only be used on Array or Hashmap iterators", 2)
     return
   end
 
@@ -94,8 +172,8 @@ function Iterator:map(fn)
 end
 
 function Iterator:filter(fn)
-  if type(self.t) ~= "table" then
-    error("[ERROR]: Iterator:filter can only be used on table iterators")
+  if type(self.obj) ~= "table" then
+    error("Iterator:filter() can only be used on Array or Hashmap iterators", 2)
     return
   end
 
