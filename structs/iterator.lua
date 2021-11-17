e88e5db54735077a48f1e1e53163f4fb1d554ceb
dturnip <alexander.xie007@gmail.com>
2021-11-17 19:20:27 +1100

    fix: fmt doc comments so they render on IntelliJ

--- structs/iterator.lua
+++ structs/iterator.lua
@@ -1,4 +1,7 @@
+local tableutils = require("fplua.utils.tableutils")
 local error = require("fplua.utils.error")
+
+table.display = tableutils.display
 
 ---@class Iterator
 local Iterator = {}
@@ -32,13 +35,13 @@ local function into_iter(t)
   end
 end
 
----Instantiates a new Iterator structure from an indexable built in data structure.
+--- Instantiates a new Iterator structure from an indexable built in data structure.
 ---```lua
---  local chars_iter = Iterator.from("fplua")
---  local array_iter = Iterator.from({ 1, 2, 3 })
---  local map_iter   = Iterator.from({ a = 1, b = 2, c = 3 })
---```
----@param t string | table
+---  local chars_iter = Iterator.from("fplua")
+---  local array_iter = Iterator.from({ 1, 2, 3 })
+---  local map_iter   = Iterator.from({ a = 1, b = 2, c = 3 })
+---```
+---@param t string|table
 ---@return Iterator
 function Iterator.from(t)
   if type(t) ~= "table" and type(t) ~= "string" then
@@ -68,36 +71,26 @@ function Iterator.from(t)
   return buf
 end
 
----Pretty prints the Iterator structure to the console.
+--- Pretty prints the Iterator structure to the console.
 ---```lua
---  local xs = Iterator.from({ 1, 2, 3 })
---  xs:dbg()
---
---  -- table: 0x... {
---  --   [fn] => function: 0x...
---  --   [state] => 0
---  --   [obj] => table: 0x... {
---  --              [1] => 1
---  --              [2] => 2
---  --              [3] => 3
---  --            }
---  --   }
---```
+---  local xs = Iterator.from({ 1, 2, 3 })
+---  xs:dbg()
+---```
 function Iterator:dbg()
   table.display(self)
 end
 
----Returns an iterator from an Iterator structure which can be used in a generic 'for-in' loop. This method is not suggested to be used directly, but can be if for whatever reason necessary.
+--- Returns an iterator from an Iterator structure which can be used in a generic 'for-in' loop. This method is not suggested to be used directly, but can be if for whatever reason necessary.
 ---```lua
---  local xs = Iterator.from({ 1, 2, 3 })
---  local sum = 0
---
---  for x in xs:iter() do
---    sum = sum + x
---  end
---
---  print(sum) --> 6
---```
+---  local xs = Iterator.from({ 1, 2, 3 })
+---  local sum = 0
+---
+---  for x in xs:iter() do
+---    sum = sum + x
+---  end
+---
+---  print(sum) --> 6
+---```
 ---@return fun(t: any, k: any): any, any
 ---@return string | table
 ---@return number | nil
@@ -105,32 +98,31 @@ function Iterator:iter()
   return self.fn, self.obj, self.state
 end
 
----Collects the Iterator back into the original data structure. This is done by accessing self.obj on an Iterator structure.
+--- Collects the Iterator back into the original data structure. This is done by accessing self.obj on an Iterator structure.
 ---```lua
---  local xs = { 1, 2, 3 }
---
---  local new_xs = Iterator.from(xs)
---		:map(function(x) return x * 2 end)
---		:map(function(x) return x + 1 end)
---		:collect() --> { 3, 5, 7 }
---```
+---  local xs = { 1, 2, 3 }
+---
+---  local new_xs = Iterator.from(xs)
+---		:map(function(x) return x * 2 end)
+---		:map(function(x) return x + 1 end)
+---		:collect() --> { 3, 5, 7 }
+---```
 ---@return string | table
 function Iterator:collect()
   return self.obj
 end
 
----Applies a function to each yielded element(s) from an Iterator. This method shouldn't be used with intent to map an Iterator, instead it is intended to be used for side effects.
+--- Applies a function to each yielded element(s) from an Iterator. This method shouldn't be used with intent to map an Iterator, instead it is intended to be used for side effects. The below example is to portray how the method works, it's an absurd way to store logins.
 ---```lua
---  local logins = Iterator.from({ a="a", b="b" })
---  -- BTW this is just an example! It is an absurd way to store login data!
---  local file = io.open("logins.txt", "w+")
---
---  logins:foreach(function(k, v)
---		file:write(string.format("username: %s, password: %s\n", k, v))
---  end)
---
---  file:close()
---```
+---  local logins = Iterator.from({ a="a", b="b" })
+---  local file = io.open("logins.txt", "w+")
+---
+---  logins:foreach(function(k, v)
+---		file:write(string.format("username: %s, password: %s\n", k, v))
+---  end)
+---
+---  file:close()
+---```
 ---@generic K, V
 ---@param fn fun(k: K, v: V | nil): void
 function Iterator:foreach(fn)
@@ -145,30 +137,28 @@ function Iterator:foreach(fn)
   end
 end
 
----Returns a new Iterator after transforming the values from an Iterator. Since Iterator is a functor, map and alike methods can be chained.
+--- Returns a new Iterator after transforming the values from an Iterator. Since Iterator is a functor, map and alike methods can be chained with any Iterator method that takes self.
 ---```lua
---  local colours = {
---    { colour = "red", hex = "FF0000" },
---    { colour = "green", hex = "00FF00" },
---    { colour = "blue", hex = "0000FF" }
---  }
---
---  local colour_map = Iterator.from(colours)
---    :map(function(pair)
---      local ret = {}
---      ret[pair.colour] = pair.hex
---      return ret
---    end)
---    :collect()
---
---  --[[
---  -- {
---  --   { "red" = "FF0000" },
---  --   { "green" = "00FF00" },
---  --   { "blue" = "0000FF" }
---  -- }
---  --]]
---```
+---  local colours = {
+---    { colour = "red", hex = "FF0000" },
+---    { colour = "green", hex = "00FF00" },
+---    { colour = "blue", hex = "0000FF" }
+---  }
+---
+---  local colour_map = Iterator.from(colours)
+---    :map(function(pair)
+---      local ret = {}
+---      ret[pair.colour] = pair.hex
+---      return ret
+---    end)
+---    :collect()
+---
+---  -- {
+---  --   { "red" = "FF0000" },
+---  --   { "green" = "00FF00" },
+---  --   { "blue" = "0000FF" }
+---  -- }
+---```
 ---@generic K, V
 ---@param fn fun(k: K, v: V | nil): any, any
 ---@return Iterator
@@ -195,14 +185,14 @@ function Iterator:map(fn)
   return Iterator.from(buf)
 end
 
----Extracts each value from the operand Iterator which returns true after being passed into the function \`fn\` and returns a new Iterator with these.
+--- Extracts each value from the operand Iterator which returns true after being passed into the function \`fn\` and returns a new Iterator with these.
 ---```lua
---  local xs = {1, 2, 3, 4, 5, 6, 7, 8}
---
---  local evens = Iterator.from(xs)
---    :filter(function(x) return x % 2 == 0 end)
---    :collect()
---```
+---  local xs = {1, 2, 3, 4, 5, 6, 7, 8}
+---
+---  local evens = Iterator.from(xs)
+---    :filter(function(x) return x % 2 == 0 end)
+---    :collect()
+---```
 ---@generic K, V
 ---@param fn fun(k: K, v: V | nil): boolean
 ---@return Iterator
