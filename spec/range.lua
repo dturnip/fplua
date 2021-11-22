9e3305dcfcf0e02867d80d425b4b9aab65ae707a
dturnip <alexander.xie007@gmail.com>
2021-11-23 09:30:16 +1100

    test: add tests for `fplua.gens.range`

--- spec/range.lua
+++ spec/range.lua
@@ -1,9 +1,28 @@
-local range = require("fplua.gens.range")
+local Iterator = require("fplua.structs.iterator")
+local range = require("fplua.gens.range").range
 
 local M = {}
 
-function M.test_foo()
-  --
+function M.test_range()
+  local xs = {}
+
+  for x in range(4) do
+    table.insert(xs, x)
+  end
+
+  for x in range(-5, 3) do
+    table.insert(xs, x)
+  end
+
+  for x in range(8, -8, -3) do
+    table.insert(xs, x)
+  end
+
+  local sum = Iterator.from(xs):foldl(function(acc, x)
+    return acc + x
+  end, 0)
+
+  assert(sum == -3)
 end
 
-M.test_foo()
+M.test_range()
